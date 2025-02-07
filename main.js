dbec7a6c69d006305b3ea8d7ca7213f535f37a80
Rubén Del Castillo Fuentes <rdcf1@alu.ua.es>
2025-02-07 19:15:20 +0100

    Trying to rescue some parts of the project

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const appDiv = document.querySelector(".app");
-    if (appDiv) {
-        appDiv.innerHTML = `
-        <h1> Rubén Del Castillo Fuentes </h1>
-        <h2> Desarrollador Software </h2>
-        `;
-    }
+const choiceArray = document.querySelectorAll(".choice");
+
+choiceArray.forEach((card) => {
+    const icon = card.querySelector(".icon"); // Selecciona el icono SVG dentro de la tarjeta
+
+    card.addEventListener("click", (event) => {
+        // Verifica si el clic se realizó en el icono o en cualquier elemento dentro del icono
+        if (event.target === icon || icon.contains(event.target)) {
+            if (card.classList.contains("expand")) {
+                // Si la tarjeta ya está expandida, la cerramos
+                card.classList.remove("expand");
+                card.classList.add("unset");
+            } else {
+                // Cerramos todas las tarjetas
+                choiceArray.forEach((element) => {
+                    element.classList.remove("expand");
+                    element.classList.add("unset");
+                });
+                // Expandimos la tarjeta clickeada
+                card.classList.remove("unset");
+                card.classList.add("expand");
+            }
+        }
+    });
 });
